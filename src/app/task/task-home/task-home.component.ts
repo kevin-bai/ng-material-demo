bdbff6511a850f1443a2c078a83629a4c9ea5a21
kevin-bai <897840029@qq.com>
2018-06-23 13:01:43 +0800

    mock数据

--- src/app/task/task-home/task-home.component.ts
+++ src/app/task/task-home/task-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-task-home',
@@ -7,7 +7,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskHomeComponent implements OnInit {
 
-  constructor() { }
+  lists = [
+    {
+      id: 1,
+      name: '待办',
+      tasks: [
+        {
+          id: 1,
+          desc: '任务一:去星巴克买杯咖啡',
+          owner: {
+            id: 1,
+            name: '张三',
+            avatar: 'avatars:svg-11'
+          },
+          dueDate: new Date()
+        },
+        {
+          id: 2,
+          desc: '任务二:洗澡',
+          owner: {
+            id: 2,
+            name: '李四',
+            avatar: 'avatars:svg-12'
+          },
+          dueDate: new Date()
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: '进行中',
+      tasks: [
+        {
+          id: 3,
+          desc: '任务五:去鸿基影城看电影',
+          owner: {
+            id: 1,
+            name: '张三',
+            avatar: 'avatars:svg-11'
+          },
+          dueDate: new Date()
+        },
+        {
+          id: 4,
+          desc: '任务六:吃饭',
+          owner: {
+            id: 2,
+            name: '李四',
+            avatar: 'avatars:svg-12'
+          },
+          dueDate: new Date()
+        }
+      ]
+    }
+  ]
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
